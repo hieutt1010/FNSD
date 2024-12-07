480bc3ca4eb5c898691228e8f9226d44b9e02be4
hieutt1010 <trtrghieu@gmail.com>
2024-12-07 16:43:46 +0700

    remove /app

--- app/error_handlers.py
+++
@@ -1,4 +0,0 @@
-from flask import jsonify
-
-def not_found_error(error):
-    return jsonify({"message": "Resource not found"}), 404
--- app/utils.py
+++
@@ -1,5 +0,0 @@
-def check_required_fields(data, fields):
-    for field in fields:
-        if field not in data:
-            return False
-    return True
